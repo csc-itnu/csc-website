abc8b74dff84c594f68071e7bd7198d7d22a4446
prakhar-thecoder <pnp14072005@gmail.com>
2025-11-28 16:23:30 +0530

    fix: social media texts and links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 {/* linkedin */}
-                <a href="https://www.linkedin.com/in/csc-itnu-41293534a/" className="text-gray-400 hover:text-green-400 transition-colors duration-200" target="_blank">
+                <a href="https://www.linkedin.com/company/cyber-security-club-itnu/" className="text-gray-400 hover:text-green-400 transition-colors duration-200" target="_blank">
                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
                 </a>
               </div>
